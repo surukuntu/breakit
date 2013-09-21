733308a86873d21c44f13de5eac88a9a82de3a39
Sri <sri.surukuntu@pearson.com>
2013-09-21 13:17:13 -0700

    Updated to stop the servo on more light

--- sketch_sep18a/sketch_sep18a.ino
+++ sketch_sep18a/sketch_sep18a.ino
@@ -2,14 +2,17 @@
 
 Servo myservo;
 
-int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorPin = A0; // select the input pin for the potentiometer
+int transistorPin = A1;
 int potPin = 13;
 int ledPin = 22; // select the pin for the LED
 int potAngle = 0;
 int angle = 0;
+boolean servoShouldRun = true;
 
 
 int sensorValue = 0;  // variable to store the value coming from the sensor
+int transistorValue = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -31,12 +34,19 @@ void loop() {
   //Serial.println(sensorValue);  
 
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  // resister value: 2 
+  // resister value: 3.3K ohms
   float voltage = sensorValue * (5.0 / 1023.0);
   
-  if(voltage<=2){
+  transistorValue = analogRead(transistorPin);
+  float transistorVoltage = transistorValue * (5.0 / 1023.0);
+  Serial.println(transistorVoltage);
+  Serial.println(servoShouldRun);
+  
+  if(servoShouldRun && transistorVoltage<=2){
     myservo.write(angle);
     delay(15);
+  }else{
+      servoShouldRun = false;
   }
         
   potAngle = map(sensorValue, 0, 1023, 0, 179);
@@ -45,7 +55,7 @@ void loop() {
   if(voltage > 2){
     digitalWrite(ledPin, HIGH);  
   }else{
-    // turn the ledPin off:        
+    //turn the ledPin off:        
     digitalWrite(ledPin, LOW);   
   }
 }
