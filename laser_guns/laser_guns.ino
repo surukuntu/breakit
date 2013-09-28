ca98f64a0a70bc64e27dfb6065932a1f26b9a4c2
Russ Bonsall <sundevilalum@Russells-MacBook-Pro.local>
2013-09-28 13:36:23 -0700

    laser trigger / midi code

--- laser_guns/laser_guns.ino
+++ laser_guns/laser_guns.ino
@@ -1,18 +1,23 @@
 int laserLightLeftOutPin = 8;
-//int laserLightRightOutPin = 9;
+int laserLightRightOutPin = 9;
+
 volatile int state = LOW;
 long debouncing_time = 250; //Debouncing Time in Milliseconds
+
 volatile unsigned long lastMicrosLeft;
 volatile unsigned long lastMicrosRight;
 
 int laserLeftMidiNote = 0x2E;
-//int laserRightMidiNote = 0x2F;
+int laserRightMidiNote = 0x2F;
 
 void setup() {
   Serial.begin(31250);
+  sendNoteOn(laserLeftMidiNote);
+  sendNoteOn(laserRightMidiNote);
   pinMode(laserLightLeftOutPin, OUTPUT);
+  pinMode(laserLightRightOutPin, OUTPUT);
   attachInterrupt(0, debounceInterruptLeft, RISING);
-  //attachInterrupt(1, debounceInterrupt2, RISING);
+  attachInterrupt(1, debounceInterruptRight, RISING);
 }
 
 void loop() {
@@ -30,8 +35,8 @@ void debounceInterruptLeft() {
 
 void debounceInterruptRight() {
   if((long)(micros() - lastMicrosRight) >= debouncing_time * 1000) {
-    //playLaser(laserRightMidiNote);
-    //turnOnLaser(laserLightRightOutPin);
+    playLaser(laserRightMidiNote);
+    turnOnLaser(laserLightRightOutPin);
     lastMicrosRight = micros();
   }
 }
@@ -43,13 +48,20 @@ void turnOnLaser(int laserPin) {
 }
 
 void playLaser(int note) {
+  sendNoteOn(note);
+  sendNoteOff(note); 
+}
+
+void sendNoteOn(int note) {
   // Channel: 0x90, Note (B1flat): 0x2E, Velocity: 0x7E 
   // send note ON
   Serial.write(0x90);
   Serial.write(note);
-  Serial.write(0x7E);
- 
-  // send a note OFF
+  Serial.write(0x7E);  
+}
+
+void sendNoteOff(int note) {
+   // send a note OFF
   Serial.write(0x80);
   Serial.write(note);
   Serial.write(0x00); 
