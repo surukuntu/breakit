caf6d69cb6728e6a50a8e57f7a3e3949db421e5e
Russ Bonsall <russ.bonsall@pearson.com>
2013-09-29 18:37:41 -0700

    cleanup some

--- servos_in_arrays/servos_in_arrays.ino
+++ servos_in_arrays/servos_in_arrays.ino
@@ -3,7 +3,8 @@
 Servo servo0, servo1, servo2, servo3, servo4, servo5, servo6, servo7, servo8, servo9, servo10, servo11;
 
 // Odd number indexes will be for 1 player, even number index for the other
-int photoTransistorPins[12] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
+//int photoTransistorPins[12] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
+int photoTransistorPins[12] = {A11, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0};
 // Servo indexes 0-3 are up-down servos
 // Servo indexes 4-11 are left-right servos
 Servo servos[12] = {servo0, servo1, servo2, servo3, servo4, servo5, servo6, servo7, servo8, servo9, servo10, servo11};
@@ -42,7 +43,7 @@ void setup() {
 void loop() {
   int i = 0;
   for(i = 0; i < 12; i++) {
-    //delay(10);
+    delay(10);
     setPhotoTransitorHitStatus(i);
     setNextAngle(i);    
   }
@@ -67,38 +68,24 @@ void setPhotoTransitorHitStatus(int index) {
 }
 
 void setNextAngle(int index) {
-  //Serial.print("setNextAngle index: ");
-  //Serial.println(index);
-  //Serial.print("photoTransistorHit: ");
-  //Serial.println(photoTransistorHit[index]);
 
-  Serial.println(index);
+  //Serial.println(index);
   // determine if it should be moving
   if(photoTransistorHit[index]) {
-    Serial.println("Knows it's hit");
+    Serial.print("Knows it's hit");
+    Serial.println(index);
     return;       
   }
   
   int maxAngle = maxServoAngle;
-  if(index<=3){
+  if(index <= 3){
     maxAngle = maxUpDownServoAngle;
   }
    
   if(servoAngles[index] == maxAngle || servoAngles[index] == 0){
-      //Serial.println("Switching direction");
       servoDirections[index] =  servoDirections[index] * -1; // flip direction (1 or -1)
   }
-  //Serial.print("servoDirections[index]: ");
-  //Serial.println(servoDirections[index]);
+
   servoAngles[index] += servoDirections[index];
-  
-  //Serial.print("servoAngles: ");
-  //Serial.println(servoAngles[index]);
-  servos[index].write(servoAngles[index]);
-  
-  //Serial.print("servos[index]");
-  if(index == 11){
-    //Serial.println(servos[index].read());
-  }
-    
+  servos[index].write(servoAngles[index]);    
 }
